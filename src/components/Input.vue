1ec266abe9d91ab3bf351437fe74228e619ad41c
Joelle Helgert <joelle.helgert@gmail.com>
2021-06-18 15:28:23 +0200

    Adding vue router

---
+++ src/components/Input.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="text" />
+    <button>Zur Terminauswahl</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>VRS Masterarbeitsstudie Joelle</h1>
+    <p>Vielen Dank erstmal, dass du an meiner User Study teilnimmst!🥳</p>
+    <p>
+      Im Rahmen der Studie musst du mit meiner selbst gebauten Messstudie 3
+      verschiedene Anwendungen testen und davor und danach einen Fragebogen
+      ausfüllen. Das dauert ca. 30 Minuten pro Anwendung. Das einzige "Problem"
+      ist, dass du zwischen den Anwendungen eine Pause von mindestens 60 Minuten
+      machen musst. Deshalb würde ich dich bitten, dir im nächsten Schritt 3
+      Termine auszusuchen, an denne du Zeit und Lust hast das ganze zu machen.
+      Die User Study selbst findet am FH Campus Hagenberg (FH 2 Ebene 3 Raum
+      ---) statt. Je nach Anreise etc kannst du gern in den Wartezeiten bei uns
+      zu Hause im Garten etwas entspannen. Für Snacks und Getränke ist auf jeden
+      Fall gesorgt!
+    </p>
+
+    <Input msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Input from "@/components/Input.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Input,
+  },
+};
+</script>
